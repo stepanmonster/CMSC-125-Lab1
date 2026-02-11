7d5160184136fad8b7162832b3bbfb5d5a8f79c7
stepanmonster <sgniedes@up.edu.ph>
2026-02-11 11:19:11 +0800

    Added EOF handling on main loop

--- src/mysh.c
+++ src/mysh.c
@@ -6,30 +6,7 @@
 #include <stdlib.h>
 #include "command.h"
 // #include <parser.c> or makefile
-
-// char *builtins[] = {"exit", "cd", "pwd"};
-
-// void exitShell(void);
 void testPrint(char *args[], int i);
-
-// ignore (in progress)
-/* int checkIfBuiltIn(char *input){
-    for(int i = 0; i < sizeof(builtins[0]); i++){
-        if(strcmp(input, builtins[0])){
-            return 0;
-        } else if (strcmp(input, builtins[1])){
-            return 1;
-        } else if (strcmp(input, builtins[2])){
-            return 2;
-        }
-    }
-} */
-
-// void testPrint(char *args[], int i){
-//     for(int j = 0; j < i; j++){
-//             printf("%s \n", args[j]);
-//     }
-// }
 
 int main(){
     // note: build switch case for built ins
@@ -49,7 +26,10 @@ int main(){
         bool background = false;
 
         printf("mysh> ");
-        getline(&input, &len, stdin);
+        if (getline(&input, &len, stdin) == -1) {
+            printf("\nExiting...\n");
+            break;
+        }
 
         char* token = strtok(input, " \n");
         if(token == NULL){ continue; };
